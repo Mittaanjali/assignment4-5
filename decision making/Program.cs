084bd267eb59de61894031f1b999921d72f27edc
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-09 15:54:05 +0530

    decision making

---
+++ decision making/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decision_making
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your exam score as a percentage: ");
+            double examScore = double.Parse(Console.ReadLine());
+
+            if (examScore >= 90)
+            {
+                Console.WriteLine("Your grade is: A");
+            }
+            else if (examScore >= 80)
+            {
+                Console.WriteLine("Your grade is: B");
+            }
+            else if (examScore >= 70)
+            {
+                Console.WriteLine("Your grade is: C");
+            }
+            else if (examScore >= 60)
+            {
+                Console.WriteLine("Your grade is: D");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is: F");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
+    
+
